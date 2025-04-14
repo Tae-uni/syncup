71626569126b8d5fde8ee6b3d8aeb23d93ef019d
taekwon <universe134679@gmail.com>
2025-04-15 02:55:17 +0900

    :sparkles: (client) build landing page (main)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello Home!
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl">SyncUp</h1>
+      <p className="text-lg mt-8">
+        Quickly sync schedule with your group - all in just a few second!
+      </p>
+      
+      <Link href="/" className="mt-20">
+        <Button variant={"default"} className="mt-20">
+          Create a new meeting
+        </Button>
+      </Link>
     </main>
   );
 }
