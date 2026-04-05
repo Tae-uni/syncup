bcbd1d6071dd88f4dc1d8082c58477f4501430c1
taekwon <universe134679@gmail.com>
2026-04-05 16:33:12 +0900

    :sparkles: (client) apply Geist Sans font and Indigo color theme

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
+import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SyncUp",
@@ -16,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className={GeistSans.variable}>
+      <body className={GeistSans.className}>
         {children}
         <Toaster richColors position="top-center" />
       </body>
