5a697d9b8b1e4d9943539ce56bd04d9215077e7a
taekwon <universe134679@gmail.com>
2026-06-26 23:13:24 +0900

    :sparkles: (server) add 413 response and update Swagger constraints

--- server/src/middlewares/validators.ts
+++ server/src/middlewares/validators.ts
@@ -26,6 +26,13 @@ export const validateRequest =
         return next();
       } catch (error) {
         if (error instanceof ZodError) {
+          const tooManyOptions = error.errors.some(
+            e => e.code === 'too_big' && e.path[0] === 'timeSelector'
+          );
+          if (tooManyOptions) {
+            return next(new AppError("Too many time options", 413, "TOO_MANY_TIME_OPTIONS"));
+          }
+
           const details = error.errors.map((e) => ({
             path: e.path.join('.'),
             message: e.message
